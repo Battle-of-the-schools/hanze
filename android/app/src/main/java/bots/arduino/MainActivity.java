b5533a936e98e337204950d9018da36d5485af03
hilkojj <hilkojj@outlook.com>
2018-10-31 16:15:15 +0100

    added Physicaloid dependency

--- android/app/src/main/java/bots/arduino/MainActivity.java
+++ android/app/src/main/java/bots/arduino/MainActivity.java
@@ -1,18 +1,14 @@
 package bots.arduino;
 
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.physicaloid.lib.Physicaloid;
 
 public class MainActivity extends AppCompatActivity {
+
+	Physicaloid mPhysicaloid; // initialising library
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,32 +16,15 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		System.out.print("gestart");
 
-		UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-		HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
-
-		System.out.print(usbDevices.size());
-		System.out.print(usbDevices.toString());
 
 
-		if (!usbDevices.isEmpty()) {
-			for (UsbDevice device : usbDevices.values()) {
-				int deviceVID = device.getVendorId();
-				if (deviceVID == 0x2341)//Arduino Vendor ID
-				{
-					System.out.print("found arduino");
-//					PendingIntent pi = PendingIntent.getBroadcast(this, 0,
-//							new Intent(ACTION_USB_PERMISSION), 0);
-//					usbManager.requestPermission(device, pi);
-//					keep = false;
-				}
-			}
-		}
+
 
 	}
 
 
 	public void testArduino(View view) {
+
 
 
 
