2d5d54aed78329e2e1d9cf063f5ccd9314e5d58a
timostrating <timostrating@home.nl>
2018-10-31 13:51:07 +0100

    you can now create messages

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :caretaker, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
