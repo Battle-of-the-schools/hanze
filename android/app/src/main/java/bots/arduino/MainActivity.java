961a4a0be2d69301cfc28a910d2243597cb860b5
hilkojj <hilkojj@outlook.com>
2018-10-31 16:19:57 +0100

    sucessfull connection to arduino

--- android/app/src/main/java/bots/arduino/MainActivity.java
+++ android/app/src/main/java/bots/arduino/MainActivity.java
@@ -3,6 +3,7 @@ package bots.arduino;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.physicaloid.lib.Physicaloid;
 
@@ -16,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		System.out.print("gestart");
 
+		mPhysicaloid = new Physicaloid(this);
+		mPhysicaloid.setBaudrate(9600);
 
+		if (mPhysicaloid.open()) {
+
+			Toast.makeText(this, "Successfully connected", Toast.LENGTH_LONG).show();
+
+		} else {
+			Toast.makeText(this, "Cannot find Arduino", Toast.LENGTH_LONG).show();
+		}
 
 
 
