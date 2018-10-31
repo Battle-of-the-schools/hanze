38ef32f362ebcd4b29ba5316b411c9ad29729bd1
timostrating <timostrating@home.nl>
2018-10-31 13:02:53 +0100

    Added caretakers that can register and login

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :caretaker, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   root 'welcome#index'
 end
---
+++ website/test/models/caretaker_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CaretakerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
