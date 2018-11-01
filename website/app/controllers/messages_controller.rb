bfbdd0b5a7b26f9c8a85f0b4eeac2db68f684be6
timostrating <timostrating@home.nl>
2018-11-01 11:45:08 +0100

    api

--- website/app/controllers/messages_controller.rb
+++ website/app/controllers/messages_controller.rb
@@ -25,6 +25,7 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
+    puts message_params
 
     respond_to do |format|
       if @message.save
@@ -69,6 +70,6 @@ class MessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:message, :location, :latitude, :longitude)
+      params.require(:message).permit(:message, :location, :latitude, :longtitude)
     end
 end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :caretaker, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
+  post "/api/send", to: 'api#send_data'
+
   root 'welcome#index'
 end
