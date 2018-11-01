ae4bb8b7d1a31d69cefebf76152f4f011bebf458
Simon Wielenga <s.wielenga@live.nl>
2018-11-01 09:57:55 +0100

    added support for a button on pin 8

--- Arduino/ThingTest/ThingTest.ino
+++ Arduino/ThingTest/ThingTest.ino
@@ -3,10 +3,17 @@
 #define loraSerial Serial1
 #define debugSerial Serial
 
+#define button 8
+
 const char *appEui = "70B3D57ED0013EDC";
 const char *appKey = "609820B02340A427E7E7D47BCB093A22";
 
 bool buttonPressed = false;
+bool lastButtonState = false;
+
+//GPS
+float flat = 0;
+float flong = 0;
 
 // Replace REPLACE_ME with TTN_FP_EU868 or TTN_FP_US915
 #define freqPlan TTN_FP_EU868
@@ -14,6 +21,7 @@ bool buttonPressed = false;
 TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
     
 void setup() {
+  pinMode(button, INPUT_PULLUP);
   // Set callback for incoming messages
   ttn.onMessage(message);
   loraSerial.begin(57600);
@@ -25,16 +33,17 @@ void setup() {
   // Wait a maximum of 10s for Serial Monitor
   while (!debugSerial && millis() < 10000);
     
-  debugSerial.println("-- STATUS");
+  //debugSerial.println("-- STATUS");
   ttn.showStatus();
-  debugSerial.println("-- JOIN");
+  //debugSerial.println("-- JOIN");
   ttn.join(appEui, appKey);
-  debugSerial.println("-- LOOP"); // geeft aan de de loop begint
+  //debugSerial.println("-- LOOP"); // geeft aan de de loop begint
 }
 
 String inData = "";
 
 void loop() {
+  buttonHandler();
 
   while (debugSerial.available() > 0)
     {
@@ -44,8 +53,8 @@ void loop() {
         // Process message when new line character is recieved
         if (recieved == '\n')
         {
-            debugSerial.print("Arduino Received: ");
-            debugSerial.print(inData);
+            //debugSerial.print("Arduino Received: ");
+            //debugSerial.print(inData);
       
             byte data[inData.length()];
             inData.getBytes(data, inData.length());
@@ -56,14 +65,27 @@ void loop() {
         }
     }
     
-  if (buttonPressed) {
-    byte data[] = ("HELP");
+  if (buttonPressed && !lastButtonState) {
+    debugSerial.println("-- HELP");
+    String outStr = "HELP";
+    outStr.concat(',');
+    outStr.concat(String(flat));
+    outStr.concat(',');
+    outStr.concat(String(flong));
+    
     // Send it off
+    byte data[outStr.length()];
+    outStr.getBytes(data, outStr.length());
     ttn.sendBytes(data, sizeof(data));
   }
 }
 
 void message(const byte* payload, int length, int port) {
-  debugSerial.println("-- MESSAGE");
+  //debugSerial.println("-- MESSAGE");
   debugSerial.println(String((char*) payload));
+}
+
+void buttonHandler() {
+  lastButtonState = buttonPressed;
+  buttonPressed = digitalRead(button);
 }
