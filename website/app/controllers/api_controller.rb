c12d74c2d987f1eb9b0043490b0240add3569ad8
timostrating <timostrating@home.nl>
2018-11-01 14:19:16 +0100

    api improvement

--- website/app/controllers/api_controller.rb
+++ website/app/controllers/api_controller.rb
@@ -5,9 +5,7 @@ class ApiController < ApplicationController
     def send_data
         # data = '{"app_id":"thingtest","dev_id":"zender","hardware_serial":"0004A30B001BC0BA","port":1,"counter":0,"is_retry":true,"payload_raw":"SEVMUAA=","payload_fields":{"message":"HELP"},"metadata":{"time":"2018-10-31T19:41:55.838464622Z","frequency":867.9,"modulation":"LORA","data_rate":"SF7BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-b827ebfffef35e71","timestamp":1099458300,"time":"2018-10-31T19:41:55.819154Z","channel":7,"rssi":-118,"snr":0.5,"rf_chain":0,"latitude":53.20393,"longitude":6.52751,"altitude":8}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo"}'
 
-        puts params["longitude"]
-
-        if params["payload_fields"]["message"].length > 0
+        if params["payload_fields"]["message"].length > 0 && params["payload_raw"] != "AA=="
             message_params = {
                 "message" => params["payload_fields"]["message"], 
                 "location" => "haiti", 
@@ -24,64 +22,3 @@ class ApiController < ApplicationController
         end
     end
 end
-
-
-
-# {"app_id":"thingtest","dev_id":"zender","hardware_serial":"0004A30B001BC0BA","port":1,"counter":0,"is_retry":true,"payload_raw":"SEVMUAA=","payload_fields":{"message":"HELP"},"metadata":{"time":"2018-10-31T19:41:55.838464622Z","frequency":867.9,"modulation":"LORA","data_rate":"SF7BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-b827ebfffef35e71","timestamp":1099458300,"time":"2018-10-31T19:41:55.819154Z","channel":7,"rssi":-118,"snr":0.5,"rf_chain":0,"latitude":53.20393,"longitude":6.52751,"altitude":8}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo"}
-# {"app_id"=>"thingtest", "dev_id"=>"zender", "hardware_serial"=>"0004A30B001BC0BA", "port"=>1, "counter"=>0, "is_retry"=>true, "payload_raw"=>"SEVMUAA=", "payload_fields"=>{"message"=>"HELP"}, "metadata"=>{"time"=>"2018-10-31T19:41:55.838464622Z", "frequency"=>867.9, "modulation"=>"LORA", "data_rate"=>"SF7BW125", "coding_rate"=>"4/5", "gateways"=>[{"gtw_id"=>"eui-b827ebfffef35e71", "timestamp"=>1099458300, "time"=>"2018-10-31T19:41:55.819154Z", "channel"=>7, "rssi"=>-118, "snr"=>0.5, "rf_chain"=>0, "latitude"=>53.20393, "longitude"=>6.52751, "altitude"=>8}]}, "downlink_url"=>"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo", "controller"=>"api", "action"=>"send_data", "api"=>{"app_id"=>"thingtest", "dev_id"=>"zender", "hardware_serial"=>"0004A30B001BC0BA", "port"=>1, "counter"=>0, "is_retry"=>true, "payload_raw"=>"SEVMUAA=", "payload_fields"=>{"message"=>"HELP"}, "metadata"=>{"time"=>"2018-10-31T19:41:55.838464622Z", "frequency"=>867.9, "modulation"=>"LORA", "data_rate"=>"SF7BW125", "coding_rate"=>"4/5", "gateways"=>[{"gtw_id"=>"eui-b827ebfffef35e71", "timestamp"=>1099458300, "time"=>"2018-10-31T19:41:55.819154Z", "channel"=>7, "rssi"=>-118, "snr"=>0.5, "rf_chain"=>0, "latitude"=>53.20393, "longitude"=>6.52751, "altitude"=>8}]}, "downlink_url"=>"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo"}}
-# {"app_id"=>"thingtest",
-#  "dev_id"=>"zender",
-#  "hardware_serial"=>"0004A30B001BC0BA",
-#  "port"=>1,
-#  "counter"=>0,
-#  "is_retry"=>true,
-#  "payload_raw"=>"SEVMUAA=",
-#  "payload_fields"=>{"message"=>"HELP"},
-#  "metadata"=>{"time"=>"2018-10-31T19:41:55.838464622Z",
-    #  "frequency"=>867.9,
-    #  "modulation"=>"LORA",
-    #  "data_rate"=>"SF7BW125",
-    #  "coding_rate"=>"4/5",
-    #  "gateways"=>[{"gtw_id"=>"eui-b827ebfffef35e71",
-        #  "timestamp"=>1099458300,
-        #  "time"=>"2018-10-31T19:41:55.819154Z",
-        #  "channel"=>7,
-        #  "rssi"=>-118,
-        #  "snr"=>0.5,
-        #  "rf_chain"=>0,
-        #  "latitude"=>53.20393,
-        #  "longitude"=>6.52751,
-        #  "altitude"=>8}]
-# },
-#  "downlink_url"=>"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo",
-#  "controller"=>"api",
-#  "action"=>"send_data",
-#  "api"=>{"app_id"=>"thingtest",
-#  "dev_id"=>"zender", "hardware_serial"=>"0004A30B001BC0BA", "port"=>1, "counter"=>0, "is_retry"=>true, "payload_raw"=>"SEVMUAA=", "payload_fields"=>{"message"=>"HELP"}, "metadata"=>{"time"=>"2018-10-31T19:41:55.838464622Z", "frequency"=>867.9, "modulation"=>"LORA", "data_rate"=>"SF7BW125", "coding_rate"=>"4/5", "gateways"=>[{"gtw_id"=>"eui-b827ebfffef35e71", "timestamp"=>1099458300, "time"=>"2018-10-31T19:41:55.819154Z", "channel"=>7, "rssi"=>-118, "snr"=>0.5, "rf_chain"=>0, "latitude"=>53.20393, "longitude"=>6.52751, "altitude"=>8}]}, "downlink_url"=>"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo"}}
-
-
-
-# "app_id":"thingtest",
-# "dev_id":"zender",
-# "hardware_serial":"0004A30B001BC0BA",
-# "port":1,
-# "counter":0,
-# "is_retry":true,
-# "payload_raw":"SEVMUAA=",
-# "payload_fields":{"message":"HELP"},
-# "metadata":{"time":"2018-10-31T19:41:55.838464622Z",
-#     "frequency":867.9,
-#     "modulation":"LORA",
-#     "data_rate":"SF7BW125",
-#     "coding_rate":"4/5",
-#     "gateways":[{"gtw_id":"eui-b827ebfffef35e71",
-#     "timestamp":1099458300,
-#     "time":"2018-10-31T19:41:55.819154Z",
-#     "channel":7,
-#     "rssi":-118,
-#     "snr":0.5,
-#     "rf_chain":0,
-#     "latitude":53.20393,
-#     "longitude":6.52751,
-#     "altitude":8}]},
-# "downlink_url":"https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/thingtest/posttest?key=ttn-account-v2.cDXMIDC5jZJrJuX1aKbmpFUtWeMDg9XK7z4VMwnlLQo"}"}
