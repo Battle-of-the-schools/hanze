8a1e319e92c376a91028dbe909058f08b6cd156a
timostrating <timostrating@home.nl>
2018-11-02 05:32:04 +0100

    hilko magic

---
+++ website/app/assets/javascripts/sockets-but-better.js
@@ -0,0 +1,38 @@
+setInterval(() => {
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/messages.json", false);
+    xhttp.send();
+    
+    list = JSON.parse(xhttp.responseText);
+
+    for (var almostDeadHuman of list) {
+
+        console.log(almostDeadHuman);
+
+        var divId = "message-" + almostDeadHuman.id;
+        var div = document.getElementById(divId);
+
+        var html = `
+        <div class="message-item" id="${divId}">
+            <div class="row">
+                <div class="col-sm-9 message-colored-item">
+                    <a class="title" href="/messages/${almostDeadHuman.id}">
+                        ${almostDeadHuman.message}
+                    </a>
+                    <br>
+                    about 19e555e6565e6e56 hours ago
+                </div>
+            </div>
+        </div>
+        `;
+
+
+        if (div)
+            div.outerHTML = html;
+        else 
+            document.getElementById("deadhumans").append(html);
+
+    }
+
+}, 1500);
